b27b069f864eaf1f2b56562135c88ce2303a53f6
prabhupurohit <prabhu.purohit@gmail.com>
2023-06-21 17:04:34 +0530

    Added Kafka Configurations

---
+++ infrastructure/kafka/kafka-consumer/src/main/java/org/example/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,10 @@
+package org.example.kafka.consumer;
+
+import org.apache.avro.specific.SpecificRecordBase;
+
+import java.util.List;
+
+public interface KafkaConsumer<T extends SpecificRecordBase> {
+    void receive(List<T> message, List<Long> keys, List<Integer> partitions, List<Long> offsets);
+
+}
---
+++ infrastructure/kafka/kafka-producer/src/main/java/org/example/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,7 @@
+package org.example.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
